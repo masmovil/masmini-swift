891903314955c5bf2ac37bce961fca0f0f9d714b
Sebastián Varela <sebastian.varela@masmovil.com>
2023-03-30 13:10:38 +0200

    Force Error to Equatable

--- Sources/Task/TaskStatus.swift
+++ Sources/Task/TaskStatus.swift
@@ -1,4 +1,4 @@
-public enum TaskStatus<Payload: Equatable, Failure: Error>: Equatable {
+public enum TaskStatus<Payload: Equatable, Failure: Error & Equatable>: Equatable {
     case idle
     case running
     case success(payload: Payload)
@@ -6,11 +6,14 @@ public enum TaskStatus<Payload: Equatable, Failure: Error>: Equatable {
 
     public static func == (lhs: Self, rhs: Self) -> Bool {
         switch (lhs, rhs) {
-        case (.idle, .idle), (.running, .running), (.failure, .failure):
+        case (.idle, .idle), (.running, .running):
             return true
 
         case (.success(let lhsSuccess), .success(let rhsSuccess)):
             return lhsSuccess == rhsSuccess
+
+        case (.failure(let lhsFailure), .failure(let rhsFailure)):
+            return lhsFailure == rhsFailure
 
         default:
             return false
