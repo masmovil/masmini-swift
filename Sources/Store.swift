cf9a51d13587a90bf34b381b3e897ffef5336b78
Sebastián Varela <sebastian.varela@masmovil.com>
2023-04-13 16:52:38 +0200

    TaskType -> Taskable

--- Sources/Store.swift
+++ Sources/Store.swift
@@ -1,40 +1,6 @@
 import Combine
 import Foundation
 
-public protocol StoreType {
-    associatedtype State: StateType
-    associatedtype StoreController: Cancellable
-
-    var state: State { get set }
-    var dispatcher: Dispatcher { get }
-    var reducerGroup: ReducerGroup { get }
-}
-
-extension StoreType {
-    /**
-     Property responsible of reduce the `State` given a certain `Action` being triggered.
-     ```
-     public var reducerGroup: ReducerGroup {
-        ReducerGroup {[
-            Reducer(of: SomeAction.self, on: self.dispatcher) { (action: SomeAction)
-                self.state = myCoolNewState
-            },
-            Reducer(of: OtherAction.self, on: self.dispatcher) { (action: OtherAction)
-                // Needed work
-                self.state = myAnotherState
-                }
-            }
-        ]}
-     ```
-     - Note : The property has a default implementation which complies with the @_functionBuilder's current limitations, where no empty blocks can be produced in this iteration.
-     */
-    public var reducerGroup: ReducerGroup {
-        ReducerGroup {
-            []
-        }
-    }
-}
-
 public class Store<State: StateType, StoreController: Cancellable>: Publisher, StoreType {
     public typealias Output = State
     public typealias Failure = Never
