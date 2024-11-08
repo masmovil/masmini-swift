664f2386f83df60f0b2826db69d442657358688c
Adrián Ruiz Lafuente <adrian.ruizlafuente@masmovil.com>
2024-11-08 13:11:17 +0100

    Add SPM support

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "Mini",
+    platforms: [
+        .iOS("14.1"),
+        .macOS(.v11),
+        .tvOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "Mini",
+            targets: ["Mini"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Mini",
+            dependencies: [],
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "MiniSwiftTests",
+            dependencies: ["Mini"],
+            path: "Tests",
+            exclude: ["LinuxMain.swift"]
+        ),
+    ]
+)
